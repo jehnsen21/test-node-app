8ed75ec44cf21c67101b735abc9f3e729a786c2a
jehnsen21 <jehnsen.enrique@accessgroup.net.au>
2025-05-27 14:25:47 +0800

    added endpoint

--- src/app.ts
+++ src/app.ts
@@ -14,8 +14,13 @@ app.post('/orders', async (req: Request, res: Response) => {
   }
 });
 
-app.get('/orders', (_req: Request, res: Response) => {
+app.get('/api/orders', (_req: Request, res: Response) => {
   res.status(200).json({ message: 'Order received!'});
+});
+
+app.post('/api/orders/create', (_req: Request, res: Response) => {
+    const order = _req.body;
+  res.status(200).json({ order });
 });
 
 export default app;
