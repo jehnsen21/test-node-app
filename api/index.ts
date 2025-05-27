44a596d661552fde172c4a1e19dcf80358bb3477
jehnsen21 <jehnsen.enrique@accessgroup.net.au>
2025-05-27 15:34:31 +0800

    updated the tscofig excuded src folder

--- api/index.ts
+++ api/index.ts
@@ -3,11 +3,11 @@ import express, { Request, Response } from 'express';
 const app = express();
 app.use(express.json()); // Express 5 built-in JSON parser
 
-app.get('/api/test', (req: Request, res: Response) => {
+app.get('/test', (req: Request, res: Response) => {
   res.status(200).json({ message: 'Hello from Vercel API!' });
 });
 
-app.get('/api/orders', (req: Request, res: Response) => {
+app.get('/orders', (req: Request, res: Response) => {
   res.status(200).json({ message: 'Hello from Vercel API!' });
 });
 
--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/orders', async (req: Request, res: Response) => {
+app.post('/orders/create', async (req: Request, res: Response) => {
   const order = req.body;
   try {
 
@@ -12,15 +12,6 @@ app.post('/orders', async (req: Request, res: Response) => {
   } catch (error) {
     res.status(500).json({ error: 'Failed to process order' });
   }
-});
-
-app.get('/api/orders', (_req: Request, res: Response) => {
-  res.status(200).json({ message: 'Order received!'});
-});
-
-app.post('/api/orders/create', (_req: Request, res: Response) => {
-    const order = _req.body;
-  res.status(200).json({ order });
 });
 
 export default app;
